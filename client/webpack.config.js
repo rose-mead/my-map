5c7b3794ff8dee0269bf23099228c6fc0eaeeff7
rose-mead <rose-mead@users.noreply.github.com>
2021-05-10 12:13:26 +1200

    remove the env thing

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,10 +17,6 @@ module.exports = {
       ignoreOrder: false // Enable to remove warnings about conflicting order
     }),
     new Dotenv(),
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
-      DEBUG: false
-    }),
   ],
   module: {
     rules: [
